6c52a2fb9fe07b171f853886052681f63000fb1b
ShiganLuo <2530320102@qq.com>
2025-09-19 11:22:56 +0800

    fix(upsetplot)：添加组合类数注释

--- Python/basic/upsetplot.py
+++ Python/basic/upsetplot.py
@@ -1,4 +1,4 @@
-# 绘制每种可能交集的情况，左图为类别元素个数，下图为所有类别交集种类，上图为对于种类元素个数
+# 绘制每种可能交集的情况，左图为类别元素个数，下图为所有类别交集种类，上图为对于种类元素个数.Cn1+Cn2+Cn3……Cnn
 mammals = ["Cat", "Dog", "Horse", "Sheep", "Pig", "Cattle", "Rhinoceros", "Moose"]
 herbivores = ["Horse", "Sheep", "Cattle", "Moose", "Rhinoceros"]
 domesticated = ["Dog", "Chicken", "Horse", "Sheep", "Pig", "Cattle", "Duck"]
