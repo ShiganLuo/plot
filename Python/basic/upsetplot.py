d9e7f90db7a665b4148598ed8db7a034e1f8ed2e
ShiganLuo <2530320102@qq.com>
2025-09-19 11:16:06 +0800

    fix(upsetplot):添加注释

--- Python/basic/upsetplot.py
+++ Python/basic/upsetplot.py
@@ -1,52 +1,13 @@
-"""
-===================================
-Basic: Examples with generated data
-===================================
+# 绘制每种可能交集的情况，左图为类别元素个数，下图为所有类别交集种类，上图为对于种类元素个数
+mammals = ["Cat", "Dog", "Horse", "Sheep", "Pig", "Cattle", "Rhinoceros", "Moose"]
+herbivores = ["Horse", "Sheep", "Cattle", "Moose", "Rhinoceros"]
+domesticated = ["Dog", "Chicken", "Horse", "Sheep", "Pig", "Cattle", "Duck"]
+(mammals, herbivores, domesticated)
+from upsetplot import from_contents,plot,UpSet
 
-This example illustrates basic plotting functionality using generated data.
-"""
-
-import matplotlib
-from matplotlib import pyplot as plt
-
-from upsetplot import generate_counts, plot
-
-example = generate_counts()
-print(example)
-
-##########################################################################
-
-plot(example)
-plt.suptitle("Ordered by degree")
-plt.show()
-
-##########################################################################
-
-plot(example, sort_by="cardinality")
-plt.suptitle("Ordered by cardinality")
-plt.show()
-
-##########################################################################
-
-plot(example, show_counts="{:,}")
-plt.suptitle("With counts shown, using a thousands separator")
-plt.show()
-
-##########################################################################
-
-plot(example, show_counts="%d", show_percentages=True)
-plt.suptitle("With counts and % shown")
-plt.show()
-
-##########################################################################
-
-plot(example, show_percentages="{:.2%}")
-plt.suptitle("With fraction shown in custom format")
-plt.show()
-
-##########################################################################
-
-matplotlib.rcParams["font.size"] = 6
-plot(example, show_percentages="{:.2%}")
-plt.suptitle("With a smaller font size")
-plt.show()
+animals = from_contents(
+    {"mammal": mammals, "herbivore": herbivores, "domesticated": domesticated}
+)
+animals
+plot(animals)
+# ax_dict = UpSet(animals, subset_size="count").plot()
